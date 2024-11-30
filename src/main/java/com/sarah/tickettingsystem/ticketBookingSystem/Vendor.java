23c2ac695b3816d1f01132d7e3b6bcefc8ba9650
sarah20232549 <sarah.20232549@iit.ac.lk>
2024-11-30 16:08:59 +0530

    added attributes, constructor, getter and setters for the vendor

--- src/main/java/com/sarah/tickettingsystem/ticketBookingSystem/Vendor.java
+++ src/main/java/com/sarah/tickettingsystem/ticketBookingSystem/Vendor.java
@@ -1,4 +1,27 @@
 package com.sarah.tickettingsystem.ticketBookingSystem;
 
 public class Vendor {
+    private String id;
+    private int ticketsSold;
+
+    public Vendor(String id, int ticketsSold){
+        this.id = id;
+        this.ticketsSold = ticketsSold;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getTicketsSold() {
+        return ticketsSold;
+    }
+
+    public void setTicketsSold(int ticketsSold) {
+        this.ticketsSold = ticketsSold;
+    }
 }
