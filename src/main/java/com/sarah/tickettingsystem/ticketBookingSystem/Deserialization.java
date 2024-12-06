41d8a33efe8d30c7197f63a2057b37fbeb48c16d
sarah20232549 <sarah.20232549@iit.ac.lk>
2024-12-06 07:35:34 +0530

    started editing deserialization

--- src/main/java/com/sarah/tickettingsystem/ticketBookingSystem/Deserialization.java
+++ src/main/java/com/sarah/tickettingsystem/ticketBookingSystem/Deserialization.java
@@ -1,4 +1,8 @@
 package com.sarah.tickettingsystem.ticketBookingSystem;
-
+import java.io.IOException;
+import java.io.File;
 public class Deserialization {
+    public static void loadJson(String[] args) {
+
+    }
 }
