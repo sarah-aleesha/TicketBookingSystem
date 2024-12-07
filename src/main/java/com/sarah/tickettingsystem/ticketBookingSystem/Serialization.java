a54f3c9c463bf87f979304163a16867ebcd6e87a
sarah20232549 <sarah.20232549@iit.ac.lk>
2024-12-07 08:16:02 +0530

    added the deserialization in the Serialization class

--- src/main/java/com/sarah/tickettingsystem/ticketBookingSystem/Serialization.java
+++ src/main/java/com/sarah/tickettingsystem/ticketBookingSystem/Serialization.java
@@ -7,7 +7,7 @@ import com.google.gson.GsonBuilder;
 public class Serialization {
 
     private static final String CONFIG_FILE = "config.json";
-    public static void saveDataToJson(TicketPool ticketPool, Customer customer, Vendor vendor) throws IOException {
+    public static void saveDataToJson(){
         Gson gson = new GsonBuilder().setPrettyPrinting().create();// creates an instance of GSON
         File file = new File(CONFIG_FILE);
         Configuration configuration = new Configuration(TicketPool.getMaximumNoOfTickets(),Vendor.getTicketReleaseRate(), Customer.getCustomerRetrievalRate(), Vendor.getTicketCount());
@@ -15,19 +15,34 @@ public class Serialization {
             try {
                 file.createNewFile();
             } catch (IOException e) {
-                System.out.println("Error occured while creating a new file. " + e.getMessage());
+                System.out.println("Error occurred while creating a new file. " + e.getMessage());
                 return;
             }
         }
         try{
             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true));
-            String toJson = gson.toJson(configuration);
-            bufferedWriter.write(toJson);
-            System.out.println("data has finaally been added to the user ine ata time");
+            /*an instance of a file writer is being wrapped around a buffered class
+            to prevent having to communicate with the file system for each character*/
+            String toJson = gson.toJson(configuration);// this converts the configuration object to JSON format to write it to the file
+            bufferedWriter.write(toJson);//writes the the Json formatted string to the file
+            System.out.println("data has been added to the file");
         } catch(IOException e){
             System.out.println("an error occurred while saving the data to a json fail");
         }
 
+    }
 
+    public static void loadDataFromJson() {
+        Gson gson = new Gson();
+        File file = new File(CONFIG_FILE);
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(CONFIG_FILE))){
+            String eachLine;
+            while((eachLine = bufferedReader.readLine()) != null){
+                System.out.println(eachLine + "\n");
+            }
+        } catch (IOException e){
+            System.out.println("an error has occurred while attempting to load data from the JSON file: ");
+            e.printStackTrace();
+        }
     }
 }
