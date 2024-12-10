28f9fd6478736ac9667d034ba6249d9276ff874f
sarah20232549 <sarah.20232549@iit.ac.lk>
2024-12-11 01:31:24 +0530

    Update Person to include a comment

--- src/main/java/com/sarah/tickettingsystem/ticketBookingSystem/CLI/Person.java
+++ src/main/java/com/sarah/tickettingsystem/ticketBookingSystem/CLI/Person.java
@@ -4,6 +4,12 @@ public class Person implements Runnable{
     private int id;
     private TicketPool ticketPool;
 
+    /*
+    polymorphism is shown in the below 2 constructors
+    if an object is initialized by giving the id and the ticket pool, the 1st constructor is called
+    on the other hand, if only the ticket pool is given, the second constructor will be called,
+    making the Person class include constructor polymorphism in its implementation
+    */
     public Person(int id, TicketPool ticketPool) {
         this.id = id;
         this.ticketPool = ticketPool;
