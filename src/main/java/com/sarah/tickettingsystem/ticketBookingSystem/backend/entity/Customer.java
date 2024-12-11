9fdbb2c431c65c688142dea257a3eab76de4f0d9
sarah20232549 <sarah.20232549@iit.ac.lk>
2024-12-11 13:12:32 +0530

    refactor: deleted unnecessary files from entity

--- src/main/java/com/sarah/tickettingsystem/ticketBookingSystem/backend/entity/Customer.java
+++
@@ -1,8 +0,0 @@
-package com.sarah.tickettingsystem.ticketBookingSystem.backend.entity;
-
-import jakarta.persistence.Entity;
-
-@Entity //specifies this class as a JPA entity. each entity will represent a row in the database
-public class Customer {
-
-}
--- src/main/java/com/sarah/tickettingsystem/ticketBookingSystem/backend/entity/Vendor.java
+++
@@ -1,7 +0,0 @@
-package com.sarah.tickettingsystem.ticketBookingSystem.backend.entity;
-
-import jakarta.persistence.Entity;
-
-@Entity
-public class Vendor {
-}
