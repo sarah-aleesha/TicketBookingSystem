c97eb93d8253845deee8ae219e9e883980d11620
sarah20232549 <sarah.20232549@iit.ac.lk>
2024-12-11 22:36:46 +0530

    build: added features

--- src/main/java/com/sarah/tickettingsystem/ticketBookingSystem/backend/repository/TicketRepository.java
+++ src/main/java/com/sarah/tickettingsystem/ticketBookingSystem/backend/repository/TicketRepository.java
@@ -1,9 +1,10 @@
 package com.sarah.tickettingsystem.ticketBookingSystem.backend.repository;
 
+import com.sarah.tickettingsystem.ticketBookingSystem.backend.entity.Ticket;
 import com.sarah.tickettingsystem.ticketBookingSystem.backend.entity.TicketPool;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TicketRepository extends JpaRepository<TicketPool, Integer> {
+public interface TicketRepository extends JpaRepository<Ticket, Integer> {
 }
